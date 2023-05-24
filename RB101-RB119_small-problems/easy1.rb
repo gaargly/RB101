757e521416ed88342402c4faed350c939560632a
Lira Samanta <lirasaman@gmail.com>
2023-05-24 16:19:59 -0400

    More problems

--- RB101-RB119_small-problems/easy1.rb
+++ RB101-RB119_small-problems/easy1.rb
@@ -7,3 +7,32 @@ end
 repeat('Hello', 3)
 
 # 2. Odd
+
+def is_odd?(num)
+  num.abs % 2 != 0
+end
+
+# 3. Stringy Strings
+
+def stringy(num)
+  if num % 2 == 0
+    return "10" * (num / 2)
+  else
+    return "10" * (num / 2) + "1"
+  end
+end
+
+# 4. What's my Bonus?
+
+def calculate_bonus(num, bool)
+  bool ? (num / 2) : 0
+end
+
+# 5. Bannerizer
+def print_in_box(str)
+  puts '+-' + '-' * str.size + '-+'
+  puts '| ' + ' ' * str.size + ' |'
+  puts '|' + ' ' + str + ' ' + '|'
+  puts '| ' + ' ' * str.size + ' |'
+  puts '+-' + '-' * str.size + '-+'
+end
